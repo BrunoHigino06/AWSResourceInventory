6bb03842ca135da782ce142b874f82db0516966e
Bruno Higino de Souza <brunohigino06@gmail.com>
2022-10-19 01:33:45 -0300

    Update Resource_Inventory.py

--- Resource_Inventory.py
+++ Resource_Inventory.py
@@ -3,6 +3,6 @@ import re
 
 def Resource_Inventory():
 
-    os.system('aws configservice select-resource-config --expression "SELECT resourceId WHERE resourceType="AWS::EC2::Instance"" --configuration-aggregator-name ')
+    os.system('aws configservice select-aggregate-resource-config --expression "SELECT resourceId WHERE resourceType="AWS::EC2::Instance"" --configuration-aggregator-name test_agregator')
 
     return
