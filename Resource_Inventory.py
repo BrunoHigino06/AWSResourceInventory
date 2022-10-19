c12f786c5834c4027f95cfc618becf8f08358a16
Bruno Higino de Souza <brunohigino06@gmail.com>
2022-10-19 04:50:21 -0300

    Update Resource_Inventory.py

--- Resource_Inventory.py
+++ Resource_Inventory.py
@@ -3,13 +3,13 @@ import re
 
 def Resource_Inventory():
     ec2 = True
-    rds = False
+    rds = True
 
     if ec2 == True and rds == False:
-        os.system('aws configservice select-aggregate-resource-config --expression "SELECT resourceId,configuration.privateIpAddress,resourceType,configuration.instanceType,tags,availabilityZone,configuration.state.name,accountId,Account Name WHERE resourceType="AWS::EC2::Instance"" --configuration-aggregator-name test_agregator')
+        os.system('aws configservice select-aggregate-resource-config --expression "SELECT resourceId,resourceName,configuration.privateIpAddress,resourceType,configuration.instanceType,tags,availabilityZone,configuration.state.name,accountId,Account Name WHERE resourceType="AWS::EC2::Instance"" --configuration-aggregator-name test_agregator --output json')
 
     if ec2 == False and rds == True:
-        os.system('aws configservice select-aggregate-resource-config --expression "SELECT resourceId,configuration.privateIpAddress,resourceType,configuration.instanceType,tags,availabilityZone,configuration.state.name,accountId,Account Name WHERE resourceType="AWS::EC2::Instance"" --configuration-aggregator-name test_agregator')
+        os.system('aws configservice select-aggregate-resource-config --expression "SELECT resourceId,resourceName,configuration.privateIpAddress,resourceType,configuration.instanceType,tags,availabilityZone,configuration.state.name,accountId,Account Name WHERE resourceType="AWS::RDS::DBInstance"" --configuration-aggregator-name test_agregator --output json')
     
     if ec2 == True and rds == True:
-        os.system('')
+        os.system('aws configservice select-aggregate-resource-config --expression "SELECT resourceId,resourceName,configuration.privateIpAddress,resourceType,configuration.instanceType,tags,availabilityZone,configuration.state.name,accountId,Account Name WHERE resourceType="AWS::RDS::DBInstance" OR resourceType="AWS::EC2::Instance"" --configuration-aggregator-name test_agregator --output json')
