a0a0311d5d14160cbaf99a70cee78ee73ce11604
Bruno Higino de Souza <brunohigino06@gmail.com>
2022-10-19 04:39:47 -0300

    Update Resource_Inventory.py

--- Resource_Inventory.py
+++ Resource_Inventory.py
@@ -2,5 +2,14 @@ import os
 import re
 
 def Resource_Inventory():
+    ec2 = True
+    rds = False
 
-    os.system('aws configservice select-aggregate-resource-config --expression "SELECT resourceId,configuration.privateIpAddress,resourceType,configuration.instanceType,tags,availabilityZone,configuration.state.name,accountId,Account Name WHERE resourceType="AWS::EC2::Instance"" --configuration-aggregator-name test_agregator')
+    if ec2 == True and rds == False:
+        os.system('aws configservice select-aggregate-resource-config --expression "SELECT resourceId,configuration.privateIpAddress,resourceType,configuration.instanceType,tags,availabilityZone,configuration.state.name,accountId,Account Name WHERE resourceType="AWS::EC2::Instance"" --configuration-aggregator-name test_agregator')
+
+    if ec2 == False and rds == True:
+        os.system('aws configservice select-aggregate-resource-config --expression "SELECT resourceId,configuration.privateIpAddress,resourceType,configuration.instanceType,tags,availabilityZone,configuration.state.name,accountId,Account Name WHERE resourceType="AWS::EC2::Instance"" --configuration-aggregator-name test_agregator')
+    
+    if ec2 == True and rds == True:
+        os.system('')
