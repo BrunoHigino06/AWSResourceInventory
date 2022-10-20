8a6a4a163bdc8ff761bb343f41a4ccef12f885ea
Bruno Higino de Souza <brunohigino06@gmail.com>
2022-10-20 08:26:21 -0300

    20.10.2022

--- report.py
+++ report.py
@@ -1,15 +1,5 @@
-fp = open("report_json.json", "r")
-#reading text line by line
-text= fp.readlines()
-#searching for character to remove
-char = text[2][9]
-#removing the character by replacing it with blank
-text[1] = text[1].replace(char, "")
-
-#opeing the file in write mode
-fw = open("report_json.json", "w") 
-#writing lines one by one
-for lines in text:
-   fw.write(lines)
-#closing the file
-fw.close()
+with open('report_json.json', 'r+') as file:
+    content = file.read()
+    file.seek(0)
+    content.replace('I', 'aa')
+    file.write(content)
