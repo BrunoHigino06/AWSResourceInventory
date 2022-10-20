bfe34cf4979c2f3ea398aa46529634cd0234f33d
Bruno Higino de Souza <brunohigino06@gmail.com>
2022-10-20 07:38:40 -0300

    20.10.2022

--- report.py
+++ report.py
@@ -1,3 +1,15 @@
-import json
+fp = open("report_json.json", "r")
+#reading text line by line
+text= fp.readlines()
+#searching for character to remove
+char = text[2][9]
+#removing the character by replacing it with blank
+text[1] = text[1].replace(char, "")
 
-
+#opeing the file in write mode
+fw = open("report_json.json", "w") 
+#writing lines one by one
+for lines in text:
+   fw.write(lines)
+#closing the file
+fw.close()
