e7fb6982658ca86309282fb4988551e4d432b8c9
Bruno Higino de Souza <brunohigino06@gmail.com>
2022-10-20 02:44:06 -0300

    Update Resource_Inventory.py

--- Resource_Inventory.py
+++ Resource_Inventory.py
@@ -2,13 +2,16 @@ import os
 
 def Resource_Inventory():
     ec2 = True
-    rds = True
+    rds = False
 
     if ec2 == True and rds == False:
-        os.system('aws configservice select-aggregate-resource-config --expression "SELECT resourceId,resourceName,configuration.privateIpAddress,resourceType,configuration.instanceType,tags,availabilityZone,configuration.state.name,accountId,Account Name WHERE resourceType="AWS::EC2::Instance"" --configuration-aggregator-name test_agregator --output json')
+        os.system("aws configservice select-aggregate-resource-config --expression \"SELECT resourceId,resourceName,configuration.privateIpAddress,resourceType,configuration.instanceType,tags,availabilityZone,configuration.state.name,accountId WHERE resourceType='AWS::EC2::Instance' --configuration-aggregator-name test_agregator --output json > report_json.json\"")
 
     if ec2 == False and rds == True:
-        os.system('aws configservice select-aggregate-resource-config --expression "SELECT resourceId,resourceName,configuration.privateIpAddress,resourceType,configuration.instanceType,tags,availabilityZone,configuration.state.name,accountId,Account Name WHERE resourceType="AWS::RDS::DBInstance"" --configuration-aggregator-name test_agregator --output json')
+        os.system("aws configservice select-aggregate-resource-config --expression \"SELECT resourceId,resourceName,configuration.privateIpAddress,resourceType,configuration.instanceType,tags,availabilityZone,configuration.state.name,accountId WHERE resourceType='AWS::RDS::DBInstance' --configuration-aggregator-name test_agregator --output json > report_json.json\"")
     
     if ec2 == True and rds == True:
-        os.system('aws configservice select-aggregate-resource-config --expression "SELECT resourceId,resourceName,configuration.privateIpAddress,resourceType,configuration.instanceType,tags,availabilityZone,configuration.state.name,accountId,Account Name WHERE resourceType="AWS::RDS::DBInstance" OR resourceType="AWS::EC2::Instance"" --configuration-aggregator-name test_agregator --output json')
+        os.system("aws configservice select-aggregate-resource-config --expression \"SELECT resourceId,resourceName,configuration.privateIpAddress,resourceType,configuration.instanceType,tags,availabilityZone,configuration.state.name,accountId WHERE resourceType='AWS::RDS::DBInstance' OR resourceType='AWS::EC2::Instance' --configuration-aggregator-name test_agregator --output json > report_json.json\"")
+
+
+Resource_Inventory()
